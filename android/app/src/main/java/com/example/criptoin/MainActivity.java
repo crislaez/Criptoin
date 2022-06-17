b4a60b2342e9685386e5a8efaac303252e08c001
crislaez <crislaez30@gmail.com>
2022-06-17 14:27:36 +0200

    change gitignore

--- android/app/src/main/java/com/example/criptoin/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.example.criptoin;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  baseEndpoint: 'https://api.coingecko.com/api/v3',
-  perPage: 30
-};
